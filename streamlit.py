153a3e1b562be65e45b54592b7f1c49eb3c2a2f9
vibcodes <85565393+vibcodes@users.noreply.github.com>
2022-06-04 16:57:25 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -10,14 +10,14 @@ import zipfile
 with zipfile.ZipFile('best_model.zip', 'r') as zip_ref:
     zip_ref.extractall()
 import os
-print(os.listdir())
+st.text(os.listdir())
 from tensorflow.keras.models import load_model
 pred_model=load_model('best_model')
 import requests, io
 url = image
 response = requests.get(url)
 img = Image.open(io.BytesIO(response.content))
-imgplot = plt.imshow(img)
+st.image(img)
 img = img.resize((224, 224))  # Resize image
 img = img.convert('RGB')
 
@@ -25,7 +25,7 @@ img = img.convert('RGB')
 img = preprocess_input(np.array(img, dtype=np.float32))
 img=np.reshape(img,(1,224,224,3))
 pred=pred_model.predict(img)
-plt.imshow(pred[0]>0.2,cmap='bone')
+st.image(pred[0]>0.2)
 pred[0]=np.where(pred[0]>0.2,1,0)
 if pred[0].sum()>0:
   st.text('Pneumonia Detected')
