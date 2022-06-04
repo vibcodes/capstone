daf297b7ffe18acbe51a84024bcacf2e4988f083
vibcodes <85565393+vibcodes@users.noreply.github.com>
2022-06-04 16:27:52 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -2,3 +2,27 @@
 import streamlit as st
   
 st.title('Capstone Project-Pneumonia Detection')
+
+
+image = st.file_uploader('Upload your xray scan here', type=None, accept_multiple_files=False, key=None, help=None, on_change=None, args=None, kwargs=None, *, disabled=False)
+
+from tensorflow.keras.models import load_model
+pred_model=load_model('best_model')
+import requests, io
+url = 'https://prod-images-static.radiopaedia.org/images/25074603/4994014ef5c834e4803541aa1dc874_jumbo.jpeg'
+response = requests.get(url)
+img = Image.open(io.BytesIO(response.content))
+imgplot = plt.imshow(img)
+img = img.resize((224, 224))  # Resize image
+img = img.convert('RGB')
+
+# Convert to float32 array
+img = preprocess_input(np.array(img, dtype=np.float32))
+img=np.reshape(img,(1,224,224,3))
+pred=pred_model.predict(img)
+plt.imshow(pred[0]>0.2,cmap='bone')
+pred[0]=np.where(pred[0]>0.2,1,0)
+if pred[0].sum()>0:
+  st.text('Pneumonia Detected')
+else:
+  st.text('No Pneumonia Detected')
